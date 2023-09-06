2d77e61311398dee9268587f515fa4310baf55b5
Ahmed Eloraby <ahmed.eloraby3547@gmail.com>
2023-09-06 11:43:16 +0300

    Overfitting

--- src/data.py
+++ src/data.py
@@ -18,17 +18,14 @@ def get_data_loaders(
     :param batch_size: size of the mini-batches
     :param valid_size: fraction of the dataset to use for validation. For example 0.2
                        means that 20% of the dataset will be used for validation
-    :param num_workers: number of workers to use in the data loaders. Use -1 to mean
-                        "use all my cores"
+    :param num_workers: number of workers to use in the data loaders. Use num_workers=1. 
     :param limit: maximum number of data points to consider
     :return a dictionary with 3 keys: 'train_one_epoch', 'valid' and 'test' containing respectively the
             train_one_epoch, validation and test data loaders
     """
 
     if num_workers == -1:
-        # Use all cores
         num_workers = multiprocessing.cpu_count()
-
     # We will fill this up later
     data_loaders = {"train": None, "valid": None, "test": None}
 
@@ -47,28 +44,55 @@ def get_data_loaders(
     # appropriate transforms for that step
     data_transforms = {
         "train": transforms.Compose(
-            # YOUR CODE HERE
+            [
+                transforms.Resize(256),
+                transforms.RandomCrop(224),
+                transforms.RandomHorizontalFlip(0.5),
+
+                transforms.RandAugment(
+                    num_ops = 2,
+                    magnitude=9,
+                    interpolation=transforms.InterpolationMode.BILINEAR,
+                ),
+
+                transforms.ToTensor(),
+                transforms.Normalize(mean, std)
+            ]
         ),
         "valid": transforms.Compose(
-            # YOUR CODE HERE
+            [
+                transforms.Resize(256),
+                transforms.RandomCrop(224),
+
+
+                transforms.ToTensor(),
+                transforms.Normalize(mean, std)
+            ]
         ),
         "test": transforms.Compose(
-            # YOUR CODE HERE
+            [
+                transforms.Resize(256),
+                transforms.RandomCrop(224),
+
+            
+                transforms.ToTensor(),
+                transforms.Normalize(mean, std)
+            ]
         ),
     }
 
     # Create train and validation datasets
     train_data = datasets.ImageFolder(
         base_path / "train",
-        # YOUR CODE HERE: add the appropriate transform that you defined in
-        # the data_transforms dictionary
+        transforms = data_transforms["train"]
+        
     )
     # The validation dataset is a split from the train_one_epoch dataset, so we read
     # from the same folder, but we apply the transforms for validation
     valid_data = datasets.ImageFolder(
         base_path / "train",
-        # YOUR CODE HERE: add the appropriate transform that you defined in
-        # the data_transforms dictionary
+        transforms = data_transforms["valid"]
+        
     )
 
     # obtain training indices that will be used for validation
@@ -85,7 +109,7 @@ def get_data_loaders(
 
     # define samplers for obtaining training and validation batches
     train_sampler = torch.utils.data.SubsetRandomSampler(train_idx)
-    valid_sampler  = # YOUR CODE HERE
+    valid_sampler  = torch.utils.data.SubsetRandomSampler(valid_idx)
 
     # prepare data loaders
     data_loaders["train"] = torch.utils.data.DataLoader(
@@ -95,13 +119,16 @@ def get_data_loaders(
         num_workers=num_workers,
     )
     data_loaders["valid"] = torch.utils.data.DataLoader(
-        # YOUR CODE HERE
+        valid_data,
+        batch_size=batch_size,
+        sampler=valid_sampler,
+        num_workers=num_workers,
     )
 
     # Now create the test data loader
     test_data = datasets.ImageFolder(
         base_path / "test",
-        # YOUR CODE HERE (add the test transform)
+        transforms = data_transforms["test"]
     )
 
     if limit > 0:
@@ -111,7 +138,11 @@ def get_data_loaders(
         test_sampler = None
 
     data_loaders["test"] = torch.utils.data.DataLoader(
-        # YOUR CODE HERE (remember to add shuffle=False as well)
+        test_data,
+        batch_size=batch_size,
+        sampler=test_sampler,
+        num_workers=num_workers,
+        shuffle=False
     )
 
     return data_loaders
@@ -126,13 +157,13 @@ def visualize_one_batch(data_loaders, max_n: int = 5):
     :return: None
     """
 
-    # YOUR CODE HERE:
+
     # obtain one batch of training images
     # First obtain an iterator from the train dataloader
-    dataiter  = # YOUR CODE HERE
+    dataiter  =  iter(data_loaders['train'])
     # Then call the .next() method on the iterator you just
     # obtained
-    images, labels  = # YOUR CODE HERE
+    images, labels  = next(dataiter)
 
     # Undo the normalization (for visualization purposes)
     mean, std = compute_mean_and_std()
@@ -145,9 +176,9 @@ def visualize_one_batch(data_loaders, max_n: int = 5):
 
     images = invTrans(images)
 
-    # YOUR CODE HERE:
+
     # Get class names from the train data loader
-    class_names  = # YOUR CODE HERE
+    class_names  = data_loaders['train'].dataset.classes
 
     # Convert from BGR (the format used by pytorch) to
     # RGB (the format expected by matplotlib)
@@ -182,7 +213,7 @@ def test_data_loaders_keys(data_loaders):
 def test_data_loaders_output_type(data_loaders):
     # Test the data loaders
     dataiter = iter(data_loaders["train"])
-    images, labels = dataiter.next()
+    images, labels = next(dataiter)
 
     assert isinstance(images, torch.Tensor), "images should be a Tensor"
     assert isinstance(labels, torch.Tensor), "labels should be a Tensor"
@@ -192,7 +223,7 @@ def test_data_loaders_output_type(data_loaders):
 
 def test_data_loaders_output_shape(data_loaders):
     dataiter = iter(data_loaders["train"])
-    images, labels = dataiter.next()
+    images, labels = next(dataiter)
 
     assert len(images) == 2, f"Expected a batch of size 2, got size {len(images)}"
     assert (
--- src/model.py
+++ src/model.py
@@ -6,19 +6,61 @@ import torch.nn as nn
 class MyModel(nn.Module):
     def __init__(self, num_classes: int = 1000, dropout: float = 0.7) -> None:
 
-        super().__init__()
+        # super().__init__()
+        super(MyModel, self).__init__()
 
         # YOUR CODE HERE
         # Define a CNN architecture. Remember to use the variable num_classes
         # to size appropriately the output of your classifier, and if you use
         # the Dropout layer, use the variable "dropout" to indicate how much
         # to use (like nn.Dropout(p=dropout))
+        # Input: 3x224x224
+        self.model = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=24, kernel_size=(5,3), stride = (1,1), padding=(2,1)),
+            nn.BatchNorm2d(24),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            # 24x112x112
+            
+            nn.Conv2d(in_channels=24, out_channels=62, kernel_size=(5,3), stride = (1,1), padding=(2,1)),
+            # nn.BatchNorm2d(62),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            # 62x56x56
+                
+            nn.Conv2d(in_channels=62, out_channels=128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            # 64x28x28
+            
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1),
+            # nn.BatchNorm2d(256),
+            nn.ReLU(),
+            # nn.MaxPool2d(2, 2),
+            # 128x14x14
+            
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(),
+            # nn.MaxPool2d(2, 2),
+            # 256x14x14
+            
+            
+            nn.Flatten(),
+            nn.Linear(512*28*28, 1000), 
+            nn.BatchNorm1d(1000),
+            nn.ReLU(),
+            nn.Linear(1000, num_classes),
+            nn.Dropout(dropout),
+
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         # YOUR CODE HERE: process the input tensor through the
         # feature extractor, the pooling and the final linear
         # layers (if appropriate for the architecture chosen)
-        return x
+        return self.model(x)
 
 
 ######################################################################################
